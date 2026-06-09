01a52700fa0fbe5fd0a8db38a12b6f16129ad88d
Max <adjoua94@gmail.com>
2026-06-09 09:04:20 +0000

    Exercices JavaScript : asynchrone, Promises, async/await

--- javascript/07-asynchrone.js
+++ javascript/07-asynchrone.js
@@ -1,3 +1,24 @@
-console.log("1 - Début")
-setTimeout(() => console.log("2 - Dans le timeout"), 2000)
-console.log("3 - Fin")
+const attendreEtRetourner = (message) => {
+    return new Promise ((resolve,reject) => {
+        setTimeout(() => {
+            message ? resolve(message) : reject('erreur!');
+        }, 1000)
+    })
+}
+
+// attendreEtRetourner('valide!').then((result) => {
+//     console.log('Resultat: ',result)
+// }).catch((err) => {
+//     console.log('erreur: ',err)
+// })
+
+const main = async () => {
+    try {
+        const resultat = await attendreEtRetourner('valide!')
+        console.log(resultat)
+    } catch (err) {
+        console.error("Une erreur est survenue :", err);
+    }
+}
+
+main()
